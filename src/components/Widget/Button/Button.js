1fa42cb9c1d592ceaa8738c5b8d16ad7369fa924
Zhe Wei <weizhe9121@sina.com>
2016-04-20 19:48:17 +0800

    Update Button component

--- src/components/Widget/Button/Button.js
+++ src/components/Widget/Button/Button.js
@@ -1,30 +1,32 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import withStyles from '../../../../node_modules/isomorphic-style-loader/lib/withStyles';
 import s from './Button.scss';
 
-class Button extends Component {
-
-  render() {
-    var buttonToDisplay;
-    if(this.props.buttonType == "primary") {
-      buttonToDisplay = <button type="button" className={s.primaryButton}>{this.props.buttonText}</button>
-    }
-    else if(this.props.buttonType == "info") {
-      buttonToDisplay = <button type="button" className={s.infoButton}>{this.props.buttonText}</button>
-    }
-    else if(this.props.buttonType == "warning") {
-      buttonToDisplay = <button type="button" className={s.warningButton} disabled="true">{this.props.buttonText}</button>
-    }
-    else if(this.props.buttonType == "default") {
-      buttonToDisplay = <button type="button" className={s.defaultButton} disabled="true">{this.props.buttonText}</button>
-    }
-
-    return (
-      <div>
-        {buttonToDisplay}
-      </div>
-    );
+function Button({ buttonType, buttonText }) {
+  let buttonToDisplay;
+  if (buttonType === 'primary') {
+    buttonToDisplay = (<button type="button" className={s.primaryButton}>
+      {buttonText}</button>);
+  } else if (buttonType === 'info') {
+    buttonToDisplay = (<button type="button" className={s.infoButton}>
+      {buttonText}</button>);
+  } else if (buttonType === 'warning') {
+    buttonToDisplay = (<button type="button" className={s.warningButton} disabled="true">
+      {buttonText}</button>);
+  } else if (buttonType === 'default') {
+    buttonToDisplay = (<button type="button" className={s.defaultButton} disabled="true">
+      {buttonText}</button>);
   }
+  return (
+    <div>
+      {buttonToDisplay}
+    </div>
+  );
 }
+
+Button.propTypes = {
+  buttonType: PropTypes.string.isRequired,
+  buttonText: PropTypes.string.isRequired,
+};
 
 export default withStyles(s)(Button);
